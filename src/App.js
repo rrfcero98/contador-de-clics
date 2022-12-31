51a8a808631fa8c3f040f4011447909b98d2aefc
Rafael Ceron <rrfcero98@gmail.com>
2022-12-30 19:44:36 -0600

    Agregando fondo, logo y primero estilos a la página

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import './App.css';
+import freeCodeCampLogo from './imagenes/freecodecamp-logo.png'
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
+      <div className='freecodecamp-logo-contenedor'>
+        <img 
+          className='freecodecamp-logo'
+          src={freeCodeCampLogo}
+          alt='Logo de freecodecamp'
+        />
+      </div>
+      <div className='contenedor-principal'>
+      </div>
     </div>
   );
 }
